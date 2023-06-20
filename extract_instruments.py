221f748892bd78b42bec9e9eb1cd2ea5826c9727
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-06-20 10:10:22 +0300

    Add files via upload

---
+++ extract_instruments.py
@@ -0,0 +1,35 @@
+import ieugwaspy as igd
+import pandas as pd
+import ieugwaspy.backwards as backwards
+from format_data import *
+
+def extract_instruments(list_ids):
+    actual_ids = backwards.legacy_ids([list_ids])
+    #print (actual_ids)
+    tophit = igd.tophits(actual_ids)
+    #print(tophit)
+    d = pd.DataFrame(tophit)
+   #print (d)
+    d['phenotype'] = d['trait'] + " || id:" + d["id"]
+    # d = format_data(
+    #     d,
+    #     data_type="exposure",
+    #     snps=None,
+    #     phenotype_col="phenotype",
+    #     snp_col="rsid",
+    #     chr_col="chr",
+    #     pos_col="position",
+    #     beta_col="beta",
+    #     se_col="se",
+    #     eaf_col="eaf",
+    #     effect_allele_col="ea",
+    #     other_allele_col="nea",
+    #     pval_col="p",
+    #     samplesize_col="n",
+    #     min_pval=1e-200,
+    #     id_col="id"
+    # )
+    d['data_source.exposure'] = 'igd'
+    return d
+
+print(extract_instruments("ieu-a-7"))
