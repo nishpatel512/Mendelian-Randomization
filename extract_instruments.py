2888ff0480841e8a21315beb4a2e242b00c24cff
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-06-21 10:23:36 +0300

    Update extract_instruments.py

--- extract_instruments.py
+++ extract_instruments.py
@@ -9,27 +9,27 @@ def extract_instruments(list_ids):
     tophit = igd.tophits(actual_ids)
     #print(tophit)
     d = pd.DataFrame(tophit)
-   #print (d)
+    #print (d)
     d['phenotype'] = d['trait'] + " || id:" + d["id"]
-    # d = format_data(
-    #     d,
-    #     data_type="exposure",
-    #     snps=None,
-    #     phenotype_col="phenotype",
-    #     snp_col="rsid",
-    #     chr_col="chr",
-    #     pos_col="position",
-    #     beta_col="beta",
-    #     se_col="se",
-    #     eaf_col="eaf",
-    #     effect_allele_col="ea",
-    #     other_allele_col="nea",
-    #     pval_col="p",
-    #     samplesize_col="n",
-    #     min_pval=1e-200,
-    #     id_col="id"
-    # )
+    d = format_data(
+        d,
+        type="exposure",
+        snps=None, header=True,
+        phenotype_col="phenotype",
+        snp_col="rsid",
+        chr_col="chr",
+        pos_col="position",
+        beta_col="beta",
+        se_col="se",
+        eaf_col="eaf",
+        effect_allele_col="ea",
+        other_allele_col="nea",
+        pval_col="p",
+        samplesize_col="n",
+        min_pval=1e-200,
+        id_col="id"
+    )
     d['data_source.exposure'] = 'igd'
     return d
 
-print(extract_instruments("ieu-a-7"))
+#print(extract_instruments("ieu-a-2"))
