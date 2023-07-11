7e2c00fa4d9124d7155a12b39bedeff197898a0c
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-07-11 09:18:30 +0300

    Update mr_wald_ratio.py

--- mr_wald_ratio.py
+++ mr_wald_ratio.py
@@ -9,12 +9,13 @@ from extract_instruments import *
 from extract_outcome_data import *
 
 def mr_wald_ratio(b_exp, b_out, se_exp, se_out, parameters):
-    if len(b_exp) > 1:
+    if not np.isscalar(b_exp) and np.size(b_exp) > 1:
         return {'b': np.nan, 'se': np.nan, 'pval': np.nan, 'nsnp': np.nan}
-    
+
     b = b_out / b_exp
     se = se_out / np.abs(b_exp)
     pval = norm.sf(np.abs(b) / se) * 2
-    
+
     return {'b': b, 'se': se, 'pval': pval, 'nsnp': 1}
+
 
