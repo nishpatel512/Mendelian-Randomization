6586d1adfebb0f60997e7b994c2a0fd2069d2a64
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-07-16 10:12:20 +0300

    Update available_outcomes.py

--- available_outcomes.py
+++ available_outcomes.py
@@ -1,6 +1,12 @@
 import ieugwaspy as igd
 import pandas as pd
 def available_outcomes():
+    '''
+    Retrieves GWAS (Genome-Wide Association Study) information using the ieugwaspy library
+    and returns it as a Pandas DataFrame.
+    Returns:
+    df (pandas.DataFrame): DataFrame containing GWAS information, with outcomes as the index.
+    '''
     data = igd.gwasinfo()
     df = pd.DataFrame.from_dict(data, orient='index')
     return df
