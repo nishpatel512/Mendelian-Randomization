f31ef819b850ffe7fb00ee96ace46064d2b3fe1e
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-07-16 10:12:47 +0300

    Update extract_instruments.py

--- extract_instruments.py
+++ extract_instruments.py
@@ -4,13 +4,19 @@ import ieugwaspy.backwards as backwards
 from format_data import *
 
 def extract_instruments(list_ids):
-    actual_ids = backwards.legacy_ids([list_ids])
+    '''
+    Input : Takes a specified outcome as input based on id
+    Output : Returns a dataframe with only independent significant associations.
+    Description : This function searches for GWAS significant SNPs for a specified set of outcomes.
+    '''
+    #print (list_ids)
+    actual_ids = backwards.legacy_ids([list_ids]) # Converting the list_ids to actual_ids using the legacy_ids function from the backwards module
     #print (actual_ids)
     tophit = igd.tophits(actual_ids)
     #print(tophit)
     d = pd.DataFrame(tophit)
     #print (d)
-    d['phenotype'] = d['trait'] + " || id:" + d["id"]
+    d['phenotype'] = d['trait'] + " || id:" + d["id"] # Creating a new column 'phenotype' in the DataFrame by concatenating 'trait' and 'id'
     d = format_data(
         d,
         type="exposure",
@@ -29,7 +35,8 @@ def extract_instruments(list_ids):
         min_pval=1e-200,
         id_col="id"
     )
-    d['data_source.exposure'] = 'igd'
+    d['data_source.exposure'] = 'igd' # Adding a new column 'data_source.exposure' with the value 'igd' to know the source of the data
     return d
 
-#print(extract_instruments("ieu-a-2"))
+#a = (extract_instruments("ieu-a-2"))
+# print (a)
