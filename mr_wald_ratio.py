7dfa52fd98afb9db4f8ad11ebb0defe38a07ca9e
nishpatel512 <64532354+nishpatel512@users.noreply.github.com>
2023-07-20 11:42:58 +0300

    Update mr_wald_ratio.py

--- mr_wald_ratio.py
+++ mr_wald_ratio.py
@@ -9,6 +9,19 @@ from extract_instruments import *
 from extract_outcome_data import *
 
 def mr_wald_ratio(b_exp, b_out, se_exp, se_out, parameters):
+'''
+  Description: This function calculates Mendelian Randomization (MR) estimates using the Wald ratio method.
+  
+  Parameters:
+  - b_exp (array-like or scalar): Beta values of the exposure variable.
+  - b_out (array-like or scalar): Beta values of the outcome variable.
+  - se_exp (array-like or scalar): Standard errors of the exposure variable.
+  - se_out (array-like or scalar): Standard errors of the outcome variable.
+  - parameters (dict): Dictionary containing additional parameters for MR analysis.
+
+  Returns:
+  result (dict): Dictionary containing the MR estimates, standard errors, p-values, and the number of SNPs used for the analysis.
+'''
     if not np.isscalar(b_exp) and np.size(b_exp) > 1:
         return {'b': np.nan, 'se': np.nan, 'pval': np.nan, 'nsnp': np.nan}
 
